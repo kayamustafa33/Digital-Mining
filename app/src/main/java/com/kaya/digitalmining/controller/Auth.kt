25485263c0150c53c4038f0a658dbe5a85895f30
Mustafa Kaya <mustafakaya.developer@gmail.com>
2024-03-11 13:47:56 +0200

    Authentication |  User is registration to Real Time DB.

--- app/src/main/java/com/kaya/digitalmining/controller/Auth.kt
+++ app/src/main/java/com/kaya/digitalmining/controller/Auth.kt
@@ -9,18 +9,21 @@ class Auth : AuthImplementation {
     private val initFirebase = FirebaseImplementor()
 
     override fun registerUser(user: User, callback: (Boolean) -> Unit) {
-        with(initFirebase){
-            firebaseAuth?.createUserWithEmailAndPassword(user.userEmail,user.userPassword)
-                ?.addOnCompleteListener {
-                    if(it.isSuccessful){
-                        callback(true)
-                    }else {
+        with(initFirebase) {
+            firebaseAuth?.createUserWithEmailAndPassword(user.userEmail, user.userPassword)
+                ?.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        val currentUser = firebaseAuth!!.currentUser
+                        currentUser?.let {
+                            registerUserToRealTimeDB(user)
+                            callback(true)
+                        }
+                    } else {
                         callback(false)
                     }
                 }?.addOnFailureListener {
                     callback(false)
                 }
-
         }
     }
 
@@ -42,6 +45,17 @@ class Auth : AuthImplementation {
     override fun isLogin() : Boolean {
         with(initFirebase){
             return firebaseAuth?.currentUser != null
+        }
+    }
+
+    private fun registerUserToRealTimeDB(user: User) {
+        val firebaseUser = initFirebase.firebaseAuth?.currentUser
+        if (firebaseUser != null) {
+            with(initFirebase){
+                createOrOpenFirebaseDB("User")
+                val userId = firebaseUser.uid
+                databaseReference!!.child(userId).setValue(user)
+            }
         }
     }
 }
