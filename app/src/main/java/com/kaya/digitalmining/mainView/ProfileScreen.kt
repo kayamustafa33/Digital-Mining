0141bf34c42e2d518b92270a32138ee45d1cc117
Mustafa Kaya <mustafakaya.developer@gmail.com>
2024-03-11 15:44:24 +0200

    Profile Screen | Profile screen design started.

--- app/src/main/java/com/kaya/digitalmining/mainView/ProfileScreen.kt
+++ app/src/main/java/com/kaya/digitalmining/mainView/ProfileScreen.kt
@@ -1,9 +1,126 @@
 package com.kaya.digitalmining.mainView
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.kaya.digitalmining.R
 
 @Composable
 fun ProfileScreen(navController: NavController){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.geometric_bg),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.FillBounds
+        )
 
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.user),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(30.dp)
+                    .size(150.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+            Text(text = "User ID", style = TextStyle(color = Color.White, fontSize = 20.sp))
+
+            Spacer(modifier = Modifier.padding(top = 50.dp))
+        Card (
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            ),
+            shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp)
+        ){
+            Row(
+                modifier = Modifier
+                    .padding(30.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Card(
+                    modifier = Modifier.size(50.dp),
+                    colors = CardColors(Color(0xff5DADE2),Color(0xff5DADE2),Color(0xff5DADE2),Color(0xff5DADE2)),
+                    shape = RoundedCornerShape(16.dp),
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 3.dp
+                    ),
+                ) {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        Image(
+                            modifier = Modifier.size(24.dp),
+                            painter = painterResource(id = R.drawable.wallet),
+                            contentDescription = null
+                        )
+                    }
+                }
+
+                Column(
+                    modifier = Modifier
+                        .padding(start = 20.dp),
+                ) {
+                    Text(
+                        text = "Wallet",
+                        fontSize = 20.sp,
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+
+        }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun Test(){
+    ProfileScreen(navController = rememberNavController())
 }
