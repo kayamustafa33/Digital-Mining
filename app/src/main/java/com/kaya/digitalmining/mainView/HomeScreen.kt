8ac98e9c1936f9e808dbe3081aa772a1bef7a015
erhansennx <erhanfatih33@gmail.com>
2024-03-19 14:21:45 +0300

    Image Slider feature added.

--- app/src/main/java/com/kaya/digitalmining/mainView/HomeScreen.kt
+++ app/src/main/java/com/kaya/digitalmining/mainView/HomeScreen.kt
@@ -5,53 +5,63 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
+import com.bumptech.glide.integration.compose.placeholder
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.HorizontalPagerIndicator
+import com.google.accompanist.pager.calculateCurrentOffsetForPage
+import com.google.accompanist.pager.rememberPagerState
 import com.kaya.digitalmining.R
 import com.kaya.digitalmining.model.New
-import com.kaya.digitalmining.model.News
 import com.kaya.digitalmining.util.CustomProgressDialog
 import com.kaya.digitalmining.viewModel.NewsViewModel
-import io.reactivex.Observer
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.yield
+import kotlin.math.absoluteValue
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
@@ -69,9 +79,11 @@ fun HomeScreen(navController: NavController) {
         }
     }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.White)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
 
         Spacer(modifier = Modifier.height(15.dp))
 
@@ -87,18 +99,11 @@ fun HomeScreen(navController: NavController) {
             color = Color.Black,
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(start = 25.dp, top = 5.dp)
+            modifier = Modifier.padding(start = 25.dp)
         )
 
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Test",
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .padding(25.dp, 15.dp),
-            contentScale = ContentScale.FillWidth
-        )
+        Spacer(modifier = Modifier.height(15.dp))
+        ImageSlider(cryptoNewsList)
 
         SearchView()
         Spacer(modifier = Modifier.height(15.dp))
@@ -107,30 +112,45 @@ fun HomeScreen(navController: NavController) {
             CustomProgressDialog(isVisible = progressState)
         }
 
-        LazyColumn(modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 25.dp, vertical = 5.dp)) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp, vertical = 5.dp)
+        ) {
             items(cryptoNewsList.size) {
-                Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
 
-                    GlideImage(
-                        model = cryptoNewsList[it].image,
-                        contentDescription = "crypto-image",
+                    Card(
+                        shape = RoundedCornerShape(8.dp),
                         modifier = Modifier
                             .height(100.dp)
                             .width(100.dp)
-                            .padding(top = 10.dp),
-                        contentScale = ContentScale.Fit) { request ->
-                        request.error(R.drawable.ic_launcher_background)
-                            .placeholder(R.drawable.progress_animation)
+                            .padding(top = 10.dp)
+                    ) {
+                        GlideImage(
+                            model = cryptoNewsList[it].image,
+                            contentDescription = "crypto-image",
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .clip(shape = RoundedCornerShape(8.dp)),
+                            contentScale = ContentScale.Crop,
+                        ) { request ->
+                            request.placeholder(R.drawable.progress_animation)
+                                .error(R.drawable.ic_launcher_background)
+                        }
                     }
+
 
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(horizontal = 10.dp, vertical = 10.dp),
                         verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.Start) {
+                        horizontalAlignment = Alignment.Start
+                    ) {
 
                         Text(
                             text = cryptoNewsList[it].title,
@@ -192,10 +212,73 @@ fun SearchView() {
 }
 
 @Composable
-fun Progress(isVisible: Boolean) {
-    CustomProgressDialog(isVisible = isVisible)
-}
+@OptIn(ExperimentalPagerApi::class, ExperimentalGlideComposeApi::class)
+fun ImageSlider(cryptoNewsList: List<New>) {
+    val pagerState = rememberPagerState(initialPage = 0)
 
+    LaunchedEffect(Unit) {
+        while (true) {
+            yield()
+            delay(3000)
+            pagerState.animateScrollToPage(
+                page = (pagerState.currentPage + 1) % (pagerState.pageCount)
+            )
+        }
+    }
+
+    Column {
+        HorizontalPager(
+            count = cryptoNewsList.size,
+            state = pagerState,
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+        ) { page ->
+            Card(
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier
+                    .graphicsLayer {
+                        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+
+                        lerp(
+                            start = 0.85f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        ).also { scale ->
+                            scaleX = scale
+                            scaleY = scale
+                        }
+
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }
+            ) {
+                GlideImage(
+                    model = cryptoNewsList[page].image,
+                    contentDescription = "crypto-image-slider",
+                    modifier = Modifier.fillMaxSize()
+                        .height(100.dp),
+                    contentScale = ContentScale.Crop
+                ) { request ->
+                    request.error(R.drawable.ic_launcher_background)
+                        .placeholder(R.drawable.progress_animation)
+                }
+            }
+        }
+
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(16.dp)
+        )
+    }
+
+}
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
