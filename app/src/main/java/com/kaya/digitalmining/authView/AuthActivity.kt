5fc86a47e1ea5af15960ca5e7d3b112e509de0f7
kayamustafa33 <mustafakaya.developer@gmail.com>
2024-03-29 10:21:20 +0200

    Splash Screen | Splash screen integrated as a laucnher activity.

--- app/src/main/java/com/kaya/digitalmining/authView/AuthActivity.kt
+++ app/src/main/java/com/kaya/digitalmining/authView/AuthActivity.kt
@@ -1,18 +1,13 @@
 package com.kaya.digitalmining.authView
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.kaya.digitalmining.controller.Auth
-import com.kaya.digitalmining.mainView.MainActivity
 
 class AuthActivity : ComponentActivity() {
-
-    private val auth = Auth()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,15 +19,4 @@ class AuthActivity : ComponentActivity() {
             }
         }
     }
-
-    override fun onResume() {
-        super.onResume()
-        if (auth.isLogin()) {
-            Intent(this@AuthActivity, MainActivity::class.java).also {
-                startActivity(it)
-                finish()
-            }
-        }
-    }
-
 }
